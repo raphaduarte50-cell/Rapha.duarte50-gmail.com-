afd4c811c3e150164f3197582232e1cda23ed4f6
raphaduarte50-cell <rapha.duarte50@gmail.com>
2026-04-27 16:09:22 -0300

    Update app.py
    from flask import Flask
    
    app = Flask(__name__)
    
    @app.route("/")
    def home():
        return "API rodando!"
    
    if __name__ == "__main__":
        import os
        port = int(os.environ.get("PORT", 10000))
        app.run(host="0.0.0.0", port=port)

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
+from flask import Flask
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "API rodando!"
+
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
